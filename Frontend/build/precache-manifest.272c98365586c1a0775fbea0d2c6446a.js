1ced3e0283a6dafbd2ba150cb3d38c7f3722ca98
Krish <krishshah235@gmail.com>
2022-10-03 19:04:33 +0530

    Build file

---
+++ Frontend/build/precache-manifest.272c98365586c1a0775fbea0d2c6446a.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f895b3514e030cdf5fc08b2972a303eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6832011db8c7e8dbad15",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "8db7af7361f8dd73749a",
+    "url": "/static/css/main.1cf32e83.chunk.css"
+  },
+  {
+    "revision": "6832011db8c7e8dbad15",
+    "url": "/static/js/2.ec49280c.chunk.js"
+  },
+  {
+    "revision": "f30b2e90b260316d7c6147203c3db2a2",
+    "url": "/static/js/2.ec49280c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8db7af7361f8dd73749a",
+    "url": "/static/js/main.8a143dbf.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "652756a14169822c2e0ce0af4ffa05cf",
+    "url": "/static/media/Cooking.652756a1.jpg"
+  },
+  {
+    "revision": "9bd1ca809372755c9c124d7e93ac18a6",
+    "url": "/static/media/Finance.9bd1ca80.jpg"
+  },
+  {
+    "revision": "ef9daf9c32f47a3468f30f861e92b9c4",
+    "url": "/static/media/Gilroy-Medium.ef9daf9c.ttf"
+  },
+  {
+    "revision": "0ea73a5674c7b109ca8c70038d55c29a",
+    "url": "/static/media/GroupIcon.0ea73a56.jpg"
+  },
+  {
+    "revision": "96070c60618226d036e967170be028c8",
+    "url": "/static/media/Janani.96070c60.png"
+  },
+  {
+    "revision": "052b04bd80cfe5e97c85521664b38e2f",
+    "url": "/static/media/Management.052b04bd.jpg"
+  },
+  {
+    "revision": "41f9bad7e644a887636d843cb672eeeb",
+    "url": "/static/media/Photography.41f9bad7.jpg"
+  },
+  {
+    "revision": "b71a7172aff9ecd9cb9f7a01f42c97ae",
+    "url": "/static/media/Schooling.b71a7172.jpg"
+  },
+  {
+    "revision": "b1639426e4131c20ff20cb6bac474fb5",
+    "url": "/static/media/Technical.b1639426.jpg"
+  },
+  {
+    "revision": "1154afbad66a6cc3680e2c3d90f46317",
+    "url": "/static/media/admin.1154afba.png"
+  },
+  {
+    "revision": "afa403cb3cbb3d6cd745a4e16941480d",
+    "url": "/static/media/all.afa403cb.jpg"
+  },
+  {
+    "revision": "94cb11af9863126f0017469c0cf237e2",
+    "url": "/static/media/circle12.94cb11af.svg"
+  },
+  {
+    "revision": "6ef18063c0b62e6445cd79e581b88df2",
+    "url": "/static/media/stitching.6ef18063.jpg"
+  }
+]);
